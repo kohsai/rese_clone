30f8d93c7a3530bb5be77f6ddfd12268947158cd
kohsai <saishokoudai@gmail.com>
2025-01-07 18:57:49 +0900

    マイページ修正

--- src/resources/views/mypage.blade.php
+++ src/resources/views/mypage.blade.php
@@ -14,7 +14,6 @@
     <header class="header">
         <span class="icon">
             <i class="fa-solid fa-square-poll-horizontal horizontal"></i></span>
-
         <h1 class="header-ttl">Rese</h1>
 
         <!-- 戻る矢印アイコン -->
@@ -30,7 +29,6 @@
             <div class="mypage-name">
                 <h2>{{ Auth::user()->name }}さん</h2> <!-- ユーザー名を表示 -->
             </div>
-
 
     <div class="mypage-container">
     <!-- 左側: 予約情報 -->
@@ -113,38 +111,11 @@
     </form>
 
     <!-- キャンセルボタン -->
-        <div class="mypage-container">
-
-        <!-- 左側: 予約情報 -->
-            <div class="reservations">
-                    <h3>予約状況</h3>
-                    <div class="reservation-list">
-        <!-- 予約情報をループで表示 -->
-    @foreach ($reservations as $index => $reservation)
-    <div class="reservation-item">
-        <div class="reservation-header">
-            <i class="fa-solid fa-clock"></i>
-            <span class="reservation-number">予約{{ $index + 1 }}</span> <!-- 何番目の予約か表示 -->
-        </div>
-
-    <div class="reservation-details">
-        <p>店舗名: {{ $reservation->shop->name }}</p> <!-- 店舗名 -->
-
-        <p>予約日: {{ \Carbon\Carbon::parse($reservation->start_at)->format('Y年m月d日') }}</p> <!-- 予約日 -->
-
-        <p>時間: {{ \Carbon\Carbon::parse($reservation->start_at)->format('H:i') }}</p> <!-- 予約時間 -->
-
-        <p>人数: {{ $reservation->num_of_users }}人</p> <!-- 予約人数 -->
-    </div>
-
-    <!-- 予約取り消しフォーム -->
-
     <form action="{{ route('reservations.destroy', $reservation->id) }}" method="POST" style="display:inline;">
     @csrf
     @method('DELETE')
     <button type="submit" class="delete-button" onclick="return confirm('この予約を取り消しますか？')">キャンセル</button>
     </form>
-
     @endif
     </div>
     </div>
@@ -154,7 +125,6 @@
     @endif
     </div>
     </div>
-
 
     {{-- 右側: お気に入り店舗 --}}
     <div class="favorites">
@@ -166,11 +136,10 @@
             <div class="shop-image" style="background-image: url({{ $shop->image_url }});">
             </div>
             <div class="shop-info">
-
                 <h3>{{ $shop->name }}</h3>
                     <div class="shop-details">
                     <span>#{{ $shop->area->area_name }}</span>
-                    <span>#{{ $sh
+                    <span>#{{ $shop->genre->genre_name }}</span>
                     </div>
                 <form action="{{ route('shops.show', $shop) }}" method="GET">
                 <button type="submit" class="detail-button">詳しく見る</button>
